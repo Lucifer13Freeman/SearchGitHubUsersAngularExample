bf467cb3b2432bbce621d9dba4dd20799aff11b6
Lucifer13Freeman <Dmitri98LD@gmail.com>
2022-07-05 19:19:14 +0300

    commit

---
+++ src/app/services/loading.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  private loading$ = new BehaviorSubject<boolean>(false);
+
+  constructor() {}
+
+  public getLoading() {
+    return this.loading$.asObservable();
+  }
+
+  public show() {
+    this.loading$.next(true);
+  }
+
+  public hide() {
+    this.loading$.next(false);
+  }
+}
